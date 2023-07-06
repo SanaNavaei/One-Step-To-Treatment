37cc5782a214a4f37be21471edca92c797157c52
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-07-06 21:53:48 +0330

    Update test.cpp

--- Codes/test.cpp
+++ Codes/test.cpp
@@ -17,11 +17,10 @@
 #include "SendInfoHandler.hpp"
 #include "HCD_Service.hpp"
 
-class Test
-{
+class Test{
 private:
 public:
-    Test() {}
+    Test(){}
     bool test_reserve_package();
     bool test_adding_request();
     bool test_pay_money();
@@ -30,81 +29,83 @@ public:
 
 bool Test::test_reserve_package(/*check if capacity being reduced*/)
 {
-    TreatmentPackageService *TPS;
-    TreatmentPackage *package = new TreatmentPackage(2, "name", 1000, 16, 3);
+    TreatmentPackageService* TPS = new TreatmentPackageService();
+    TreatmentPackage* package = new TreatmentPackage(2, "name", 1000, 16, 3);
     TPS->addPackage(package);
     TPS->reservePackage(2);
-    if (package->getCapacity() == 2)
+    if(package->getCapacity() == 2)
         return true;
     return false;
 }
 
 bool Test::test_adding_request()
 {
-    Document *document = new Document("kindOfdisease, diseaseBackground");
-    BankCard *bank_card = new BankCard("9635456974128523", "1211");
-    Patient *patient = new Patient("maryam", "1234", "maryam@gmail.com", "09144749754", document, bank_card);
-    TreatmentPackage *package = new TreatmentPackage(2, "name", 1000, 1223, 3);
-    TreatmentPackageService *TPS;
+    Document* document = new Document("kindOfdisease, diseaseBackground");
+    BankCard* bank_card = new BankCard("9635456974128523","1211");
+    Patient * patient = new Patient("maryam", "1234", "maryam@gmail.com", "09144749754", document, bank_card);
+    TreatmentPackage* package = new TreatmentPackage(2, "name", 1000, 1223, 3);
+    TreatmentPackageService* TPS = new TreatmentPackageService();
     TPS->addPackage(package);
-    ChoosePackageHandler *CPH;
+    ChoosePackageHandler* CPH = new ChoosePackageHandler(TPS);
     CPH->ChoosePackageTreatment(patient, 2);
-    if (patient->getBill()->getDept() == 1000)
+    if(patient->getBill()->getDept() == 1000)
         return true;
     return false;
 }
 
+
+
 bool Test::test_pay_money()
 {
-    Document *document = new Document("kindOfdisease, diseaseBackground");
-    BankCard *bank_card = new BankCard("9635456974128523", "1211");
-    Patient *patient = new Patient("maryam", "1234", "maryam@gmail.com", "09144749754", document, bank_card);
-    TreatmentPackage *package = new TreatmentPackage(2, "name", 1000, 1223, 3);
-    // PatientRequest* request = new PatientRequest(123, package);
-    TreatmentPackageService *TPS;
+    Document* document = new Document("kindOfdisease, diseaseBackground");
+    BankCard* bank_card = new BankCard("9635456974128523","1211");
+    Patient * patient = new Patient("maryam", "1234", "maryam@gmail.com", "09144749754", document, bank_card);
+    TreatmentPackage* package = new TreatmentPackage(2, "name", 1000, 1223, 3);
+    TreatmentPackageService* TPS = new TreatmentPackageService();
     TPS->addPackage(package);
-    ChoosePackageHandler *CPH;
-    PatientRequest *request = CPH->ChoosePackageTreatment(patient, 2);
-    PayMoneyHandler *PMH;
+    ChoosePackageHandler* CPH = new ChoosePackageHandler(TPS);
+    PatientRequest* request = CPH->ChoosePackageTreatment(patient, 2);
+    PayMoneyHandler* PMH = new PayMoneyHandler();
     PMH->PayMoney(request, patient, 25);
-    Bill *bill = patient->getBill();
-    if (bill->getDept() == 750 && bill->getPaid() == 250)
+    Bill* bill = patient->getBill();
+    if(bill->getDept() == 750 && bill->getPaid() == 250)
         return true;
     return false;
 }
 
 bool Test::test_choose_supporter()
 {
-    ChooseSupporterHandler *CSH;
-    Document *document = new Document("kindOfdisease, diseaseBackground");
-    BankCard *bank_card = new BankCard("9635456974128523", "1211");
-    Patient *patient = new Patient("maryam", "1234", "maryam@gmail.com", "09144749754", document, bank_card);
-    TreatmentPackage *package = new TreatmentPackage(2, "name", 1000, 1223, 3);
-    // PatientRequest* request = new PatientRequest(123, package);
-    TreatmentPackageService *TPS;
+    SupporterService* supS = new SupporterService();
+    ChooseSupporterHandler* CSH = new ChooseSupporterHandler(supS);
+    Document* document = new Document("kindOfdisease, diseaseBackground");
+    BankCard* bank_card = new BankCard("9635456974128523","1211");
+    Patient * patient = new Patient("maryam", "1234", "maryam@gmail.com", "09144749754", document, bank_card);
+    TreatmentPackage* package = new TreatmentPackage(2, "name", 1000, 1223, 3);
+    TreatmentPackageService* TPS = new TreatmentPackageService();
     TPS->addPackage(package);
-    ChoosePackageHandler *CPH;
-    PatientRequest *request = CPH->ChoosePackageTreatment(patient, 2);
-    PayMoneyHandler *PMH;
+    ChoosePackageHandler* CPH = new ChoosePackageHandler(TPS);
+    PatientRequest* request = CPH->ChoosePackageTreatment(patient, 2);
+    PayMoneyHandler* PMH = new PayMoneyHandler();
     PMH->PayMoney(request, patient, 25);
-    Supporter *sup = CSH->ChooseSupporter(request, patient);
-    if (sup->getStatus() == "busy")
-        if (patient->getSup() == sup)
-            if (request->getStatus() == "sup assigned")
+    Supporter* sup = CSH->ChooseSupporter(request, patient);
+    if(sup->getStatus() == "busy")
+        if(patient->getSup() == sup)
+            if(request->getStatus() == "sup assigned")
                 return true;
     return false;
 }
 
+
 int main()
 {
-    Test *test = new Test();
-    if (!test->test_reserve_package())
+    Test* test = new Test();
+    if(!test->test_reserve_package())
         std::cout << "error in reserve_package method catched!\n";
-    else if (!test->test_adding_request())
+    else if(!test->test_adding_request())
         std::cout << "error in adding request catched!\n";
-    else if (!test->test_pay_money())
+    else if(!test->test_pay_money())
         std::cout << "error in pay money handler catched!\n";
-    else if (!test->test_choose_supporter())
+    else if(!test->test_choose_supporter())
         std::cout << "error in choose supporeter handler cathced!\n";
     else
         std::cout << "successfully done without any error!\n";
